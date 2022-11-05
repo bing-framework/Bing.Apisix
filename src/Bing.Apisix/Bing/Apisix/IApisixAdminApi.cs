a74be247663334370802bd3c44b979e1f3a82912
jian玄冰 <jianxuanhuo1@126.com>
2022-11-05 13:06:00 +0800

    init: 初始化配置

---
+++ src/Bing.Apisix/Bing/Apisix/IApisixAdminApi.cs
@@ -0,0 +1,13 @@
+﻿using WebApiClientCore;
+using WebApiClientCore.Attributes;
+
+namespace Bing.Apisix;
+
+/// <summary>
+/// Apisix 的后台管理接口
+/// </summary>
+[Timeout(1000)]
+public interface IApisixAdminApi : IHttpApi
+{
+
+}
---
+++ src/Bing.Apisix/Bing/Apisix/Model/ApisixService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Bing.Apisix.Model;
+
+/// <summary>
+/// APISIX 服务编辑信息
+/// </summary>
+public class ApisixService
+{
+    /// <summary>
+    /// 标识服务名称。可选
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// 服务描述、使用场景等。可选
+    /// </summary>
+    public string Desc { get; set; }
+
+    /// <summary>
+    /// 标识附加属性的键值对。可选
+    /// </summary>
+    public Dictionary<string,string> Labels { get; set; }
+
+}
